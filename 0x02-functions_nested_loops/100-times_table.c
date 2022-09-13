dd6e4e40e975dc341760937be59b06a786f00216
Marvelous0 <afolabiiyanu6@gmail.com>
2022-09-13 15:41:00 -0700

    print_times_table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * print_times_table - function that print n times table
+ *
+ * @n: input
+ *
+ * Return: Always 0 (Success)
+ */
+void print_times_table(int n)
+{
+
+	return (0);
+}
